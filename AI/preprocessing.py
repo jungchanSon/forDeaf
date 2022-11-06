80d838c4b1661f1f5c1280c4f0034a16bcab90e0
krhros <kimsdr1@naver.com>
2022-11-06 19:10:09 +0900

    refactoring : 전처리 부분 수정(car_horn,siren,other), 모델 구조 수정, 모델 수정

--- AI/preprocessing.py
+++ AI/preprocessing.py
@@ -14,7 +14,7 @@ import os
 
 #file_name에 해당하는 파일의 mfccs를 생성,리턴
 def make_mfccs(file_name):
-    max_pad_len = 22
+    max_pad_len = 173
     time = 1
     audio,sr = librosa.load(file_name) #sr = 22050/s
     
@@ -22,8 +22,17 @@ def make_mfccs(file_name):
     #audio(파형), sr(sampling rate), Time(초) 을 인자로 받음
     audio = trim_audio(audio,sr,time)
     
+    '''
     D = np.abs(librosa.stft(audio, n_fft=4096, win_length=4096, hop_length=1024))
     mfcc = librosa.feature.mfcc(S = librosa.power_to_db(D),sr=sr,n_mfcc=40)
+    '''
+    
+    #안드로이드 jlobrosa와 맞추기위해 최대한 간단히 mfcc변환
+    #n_fft = 512, win_length = n_fft, hop_length = 128, n_mfcc = 40
+    mfcc = librosa.feature.mfcc(y=audio,sr=sr,n_fft=512,hop_length=128,n_mfcc=40)
+    
+    #(40,173) 출력됨
+    print(mfcc.shape)
     
     #1초 미만인 mfcc에 대하여 zero padding
     pad_width = max_pad_len - mfcc.shape[1]
@@ -74,6 +83,10 @@ def make_dataframe(file_path_audio,file_path_csv):
     df = pd.read_csv(path_csv)
     features = []
     val_features = []
+    car_horn=0
+    siren=0
+    other=0
+    
     
     #폴더 1~10까지 반복
     for i in range(1,10):
@@ -84,10 +97,25 @@ def make_dataframe(file_path_audio,file_path_csv):
             
             for idx,name in enumerate(df['slice_file_name']):
                 if name == file_name:
-                    class_label = df.iloc[idx,6]
+                    # car_horn, siren 소리를 제외한 나머지소리를 other로 통일
+                    if df.iloc[idx,6] == 1:
+                        #car_horn
+                        label = 0
+                        car_horn+=1;
+                    elif df.iloc[idx,6] == 8:
+                        #siren
+                        label = 1
+                        siren+=1
+                    else:
+                        #other
+                        label = 2
+                        other+=1
+                        
+                    class_label = label
                     
             data = make_mfccs(audio_file)
             #각 폴더당 약 80개 추출하여 검증파일 생성
+            print(class_label)
             if i % 10 == 0:
                 val_features.append([data,class_label])
                 print("val = ",len(val_features))
@@ -104,6 +132,10 @@ def make_dataframe(file_path_audio,file_path_csv):
     
     print("val_feature_df.pkl 생성 완료")
     print("feature_df.pkl 생성 완료")
+    print("car_horn(label 0) = ", car_horn)
+    print("siren(label 1) = ", siren)
+    print("other(label 2) = ", other)
+    
     return
 
 #전처리 실행해주는 함수 -> 컴파일 후 콘솔에서 실행
--- AI/testing.py
+++ AI/testing.py
@@ -22,20 +22,16 @@ def model_evaluate(model_path,path_pkl):
     #path_pic = 'C:/Users/User/Desktop/학교/전남대/캡스톤디자인/feature_df.pkl'
     model = keras.models.load_model(model_path)
     feature_df = pd.read_pickle(path_pkl)
-
+    
+    #print(feature_df['class_label'].value_counts())
+    
+    print(feature_df.shape)
+    
     X = np.array(feature_df.mfccs.tolist())
     y = np.array(feature_df.class_label.tolist())
     y = to_categorical(LabelEncoder().fit_transform(y))
-
-    #모델 구조 변경할 수도.
-    n_columns = 22
-    n_row = 40
-    n_channels = 1
-    n_classes = 10
-
-    with tf.device('/cpu:0'):
-        X = tf.reshape(X,[-1,n_row,n_columns,n_channels])
-        
+    
+    print(X.shape)
     test_pre = model.predict(X,verbose=0)
     test_predicted = test_pre.argmax(axis=-1)
     
@@ -52,14 +48,14 @@ def model_evaluate(model_path,path_pkl):
 7 – jackhammer
 8 – siren
 9 – street_music
+||||| 위에서 아래로 변환함
+0 - car_horn
+1 - siren
+2 - other
 '''
 
 #모델경로와 데이터파일(pkl) 파일 경로 수정 필요
-
-##추가할 사항
-## 검증용 pkl파일 생성후 로드하여 정확도 테스트
-
-model_path = 'C:/Users/User/Desktop/학교/전남대/캡스톤디자인/For_Deaf_0.1'
+model_path = 'C:/Users/User/Desktop/학교/전남대/캡스톤디자인/test'
 path_pic = 'C:/Users/User/Desktop/학교/전남대/캡스톤디자인/val_feature_df.pkl'
 test_pre,test_predicted = testing(model_path,path_pic)
 
